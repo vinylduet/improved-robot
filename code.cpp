3f56ea61a8205efd8f56949d17c358bd94ffde0b
Donny Frank <164851427+vinylduet@users.noreply.github.com>
2024-03-25 09:16:14 +0200

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,29 @@
+int main()
+{
+
+    string keys[] = {"the", "a", "there",
+                     "answer", "any", "by",
+                     "bye", "their", "where", "when"};
+    int n = sizeof(keys) / sizeof(keys[0]);
+
+    Trie t;
+    t.root = getNode();
+    // Construct trie
+    for (int i = 0; i < n; i++)
+    {
+        t.insert(keys[i]);
+    }
+
+int main(){
+//Jai Shree Ram
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string keys[] = {"the", "a", "there", "answer", "any", "by", "bye", "their" };
+    int n = sizeof(keys)/sizeof(keys[0]);
+    struct TrieNode *root = getNode();
+
+    for (int i = 0; i < n; i++) insert(root, keys[i]);
+ 
+    char output[][32] = {"Not present in trie", "Present in trie"};
+ 
