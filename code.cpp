a1f8cb53f92d87eac7d61d12236354b127436834
Donny Frank <164851427+vinylduet@users.noreply.github.com>
2024-03-25 09:16:31 +0200

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -22,7 +22,7 @@ int main(){
     string keys[] = {"the", "a", "there", "answer", "any", "by", "bye", "their" };
     int n = sizeof(keys)/sizeof(keys[0]);
     struct TrieNode *root = getNode();
-
+//pudge
     for (int i = 0; i < n; i++) insert(root, keys[i]);
  
     char output[][32] = {"Not present in trie", "Present in trie"};
